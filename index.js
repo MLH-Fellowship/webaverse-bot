27e0b096350f582b279dc7a4e1b61dbfba60cdbe
YaHeng Su <yaheng.su1@gmail.com>
2020-06-23 15:10:19 -0600

    initial bot setup

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-require('dotenv').config()
+const dotenv = require("dotenv");
+const { Client } = require("discord.js");
+dotenv.config();
+
+const client = new Client();
+
+client.once("ready", () => console.log("Client ready"));
