da72c8e9b58f7e3b0cb802f6f5816dada1087992
Shubham Jain <shubham@sjain.dev>
2020-06-24 12:39:06 +0100

    Add 'screenshot' command to get screenshot of XRPK from registry

---
+++ constants.js
@@ -0,0 +1,9 @@
+const BASE_INSPECT_URL = 'https://xrpackage.org/inspect.html';
+const BASE_IPFS_URL = 'https://ipfs.exokit.org/ipfs/';
+const BASE_API_URL = 'http://packages.exokit.org/';
+
+module.exports = {
+  BASE_INSPECT_URL,
+  BASE_IPFS_URL,
+  BASE_API_URL,
+};
