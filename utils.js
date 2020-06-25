62843e887efec2b94fde4df4aa404b6664a7c091
Shubham Jain <shubham@sjain.dev>
2020-06-25 15:57:46 +0100

    Move extraction into helper function that takes the buffer in
    This will allow us to extract XRPKs with either a URL or Buffer.

--- utils.js
+++ utils.js
@@ -4,14 +4,18 @@ const fetch = require('node-fetch');
 async function extract(xrpkUrl) {
   const res = await fetch(xrpkUrl);
   const buffer = await res.buffer();
-  const bundle = new wbn.Bundle(buffer);
+  return _extract(buffer);
+}
 
+async function _extract(buffer) {
+  const bundle = new wbn.Bundle(buffer);
   const manifestUrl = bundle.urls.find(u => u.endsWith('manifest.json'));
   const manifest = bundle.getResponse(manifestUrl).body.toString('utf-8');
 
   return {
     urls: bundle.urls,
     manifest,
+    bundle,
   };
 }
 
