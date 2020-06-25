63423ffddf18c1a79497df96868c98d18bb9f4bb
YaHeng Su <yaheng.su1@gmail.com>
2020-06-25 12:17:15 -0600

    added show inventory command

---
+++ commands/show_inventory.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+
+const name = 'inventory';
+
+const {
+  BASE_USER_URL,
+  BASE_INSPECT_URL,
+} = require('../constants');
+
+const help =
+  'To view your inventory, type `!inventory USERNAME or click the inventory button in your application.';
+
+const execute = async (message) => {
+  const username = message.content.split(' ')[1];
+  const res = await fetch(`${BASE_USER_URL}${username}`);
+  const resp = await res.json();
+
+  if (resp.error) {
+    return message.reply(`unable to find a user named \`${username}\`.`);
+  }
+  // For each inventory item, reply with the item name and xrpackage link
+  resp.inventory.forEach((element, i) => {
+    message.channel.send({
+      embed: {
+        title: `${username} Inventory - ${element.name}`,
+        url: `${BASE_INSPECT_URL}?p=${element.name}`,
+      },
+    });
+  });
+};
+
+module.exports = {name, help, execute};
--- constants.js
+++ constants.js
@@ -1,9 +1,10 @@
 const BASE_INSPECT_URL = 'https://xrpackage.org/inspect.html';
 const BASE_IPFS_URL = 'https://ipfs.exokit.org/ipfs/';
 const BASE_API_URL = 'http://packages.exokit.org/';
-
+const BASE_USER_URL = 'https://users.exokit.org/';
 module.exports = {
   BASE_INSPECT_URL,
   BASE_IPFS_URL,
   BASE_API_URL,
+  BASE_USER_URL,
 };
