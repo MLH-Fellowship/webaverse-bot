6ecfc89bb0c78513343f27ad9519fc30d9b82208
Shubham Jain <shubham@sjain.dev>
2020-06-26 10:30:25 +0100

    Add !world command to get overview of world and its packages
    Also adds link to download the scene as a wbn file.

--- constants.js
+++ constants.js
@@ -1,13 +1,19 @@
 const BASE_INSPECT_URL = 'https://xrpackage.org/inspect.html';
+const BASE_EDIT_URL = 'https://xrpackage.org/edit.html';
+
 const BASE_IPFS_URL = 'https://ipfs.exokit.org/ipfs/';
 const BASE_API_URL = 'http://packages.exokit.org/';
 const PACKAGE_NAME_REGEX = /^[a-z0-9][a-z0-9-._~]*$/;
 const BASE_USER_URL = 'https://users.exokit.org/';
+const BASE_WORLDS_URL = 'https://worlds.exokit.org/';
 
 module.exports = {
   BASE_INSPECT_URL,
+  BASE_EDIT_URL,
+
   BASE_IPFS_URL,
   BASE_API_URL,
   PACKAGE_NAME_REGEX,
   BASE_USER_URL,
+  BASE_WORLDS_URL,
 };
